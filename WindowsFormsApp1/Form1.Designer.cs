f26a026a62183665e75f922e0d6220483365d04f
profesor <profesor>
2018-11-05 22:09:34 -0300

    Se inicio el juego

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WindowsFormsApp1
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Variable del diseñador necesaria.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Limpiar los recursos que se estén usando.
+    /// </summary>
+    /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Código generado por el Diseñador de Windows Forms
+
+    /// <summary>
+    /// Método necesario para admitir el Diseñador. No se puede modificar
+    /// el contenido de este método con el editor de código.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblSeg = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.Jellyfish;
+            this.pictureBox1.Location = new System.Drawing.Point(753, 399);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblSeg
+            // 
+            this.lblSeg.AutoSize = true;
+            this.lblSeg.Location = new System.Drawing.Point(300, 9);
+            this.lblSeg.Name = "lblSeg";
+            this.lblSeg.Size = new System.Drawing.Size(34, 13);
+            this.lblSeg.TabIndex = 1;
+            this.lblSeg.Text = "02:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblSeg);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Label lblSeg;
+  }
+}
+
--- hilos/Form1.cs
+++ hilos/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,173 +11,197 @@ using System.Windows.Forms;
 
 namespace hilos
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+
+    List<Thread> hilos = new List<Thread>();
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      Numero n1 = new Numero(1);
+      Numero n2 = new Numero(2);
+      Numero n3 = new Numero(3);
+      Numero n4 = new Numero(4);
+      n1.MuestraNumeroEvent += mostrar;
+      n2.MuestraNumeroEvent += mostrar;
+      n3.MuestraNumeroEvent += mostrar;
+      n4.MuestraNumeroEvent += mostrar;
+
+
+      hilos.Add(new Thread(new ParameterizedThreadStart(sumar)));
+      hilos.Add(new Thread(new ParameterizedThreadStart(sumar)));
+      hilos.Add(new Thread(new ParameterizedThreadStart(sumar)));
+      hilos.Add(new Thread(new ParameterizedThreadStart(sumar)));
+
+      int i = 0;
+      foreach (Thread t in hilos)
+      {
+
+        t.Start(i);
+        i++;
+      }
+    }
+
+
+    public int[] numeros = { 0, 0, 0, 0 };
+    public void sumar(object o)
+    {
+      int posicion = (int)(o);
+      while (this.numeros[posicion] < 5000)
+      {
+
+        try
+        {
+          System.Threading.Thread.Sleep(40);
+          this.numeros[posicion]++;
+          mostrar(this.numeros[posicion], posicion);
+        }
+        catch (ThreadInterruptedException e)
+        {
+
+        }
+      }
+      
+    }
+
+    public void mostrar(int num, int posicion)
     {
 
-        List<Thread> hilos = new List<Thread>();
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Numero n1 = new Numero(1);
-            Numero n2 = new Numero(2);
-            Numero n3 = new Numero(3);
-            Numero n4 = new Numero(4);
-            n1.MuestraNumeroEvent += mostrar;
-            n2.MuestraNumeroEvent += mostrar;
-            n3.MuestraNumeroEvent += mostrar;
-            n4.MuestraNumeroEvent += mostrar;
-
-
-            hilos.Add(new Thread(n1.sumar));
-            hilos.Add(new Thread(n2.sumar));
-            hilos.Add(new Thread(n3.sumar));
-            hilos.Add(new Thread(n4.sumar));
-
-            foreach (Thread t in hilos)
+      switch (posicion)
+      {
+        case 1:
+          if (this.txt1.InvokeRequired)
+          {
+            this.txt1.BeginInvoke((MethodInvoker)delegate ()
             {
+              this.txt1.Text = num.ToString();
+            });
 
-                t.Start();
-            }
-        }
-
-        public void mostrar(int num, int posicion)
-        {
-
-            switch (posicion)
+          }
+          else
+            this.txt1.Text = num.ToString();
+          break;
+        case 2:
+          if (this.txt2.InvokeRequired)
+          {
+            this.txt2.BeginInvoke((MethodInvoker)delegate ()
             {
-                case 1:
-                    if (this.txt1.InvokeRequired)
-                    {
-                        this.txt1.BeginInvoke((MethodInvoker)delegate()
-                        {
-                            this.txt1.Text = num.ToString();
-                        });
+              this.txt2.Text = num.ToString();
+            });
 
-                    }
-                    else
-                        this.txt1.Text = num.ToString();
-                    break;
-                case 2:
-                    if (this.txt2.InvokeRequired)
-                    {
-                        this.txt2.BeginInvoke((MethodInvoker)delegate()
-                        {
-                            this.txt2.Text = num.ToString();
-                        });
-
-                    }
-                    else
-                        this.txt2.Text = num.ToString();
-                    break;
-                case 3:
-                    if (this.txt3.InvokeRequired)
-                    {
-                        this.txt3.BeginInvoke((MethodInvoker)delegate()
-                        {
-                            this.txt3.Text = num.ToString();
-                        });
-
-                    }
-                    else
-                        this.txt3.Text = num.ToString();
-                    break;
-                case 4:
-                    if (this.txt4.InvokeRequired)
-                    {
-                        this.txt4.BeginInvoke((MethodInvoker)delegate()
-                        {
-                            this.txt4.Text = num.ToString();
-                        });
-
-                    }
-                    else
-                        this.txt4.Text = num.ToString();
-                    break;
-            }
-
-
-        }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            foreach (Thread t in hilos)
+          }
+          else
+            this.txt2.Text = num.ToString();
+          break;
+        case 3:
+          if (this.txt3.InvokeRequired)
+          {
+            this.txt3.BeginInvoke((MethodInvoker)delegate ()
             {
-                if (t.IsAlive)
-                    t.Abort();
+              this.txt3.Text = num.ToString();
+            });
 
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (hilos[1].IsAlive)
-                hilos[1].Abort();
-            else
+          }
+          else
+            this.txt3.Text = num.ToString();
+          break;
+        case 0:
+          if (this.txt4.InvokeRequired)
+          {
+            this.txt4.BeginInvoke((MethodInvoker)delegate ()
             {
-                Numero n1 = new Numero(2);
-                n1.Num = int.Parse(txt2.Text);
-                n1.MuestraNumeroEvent += mostrar;
+              this.txt4.Text = num.ToString();
+            });
+
+          }
+          else
+            this.txt4.Text = num.ToString();
+          break;
+      }
 
 
-                hilos[1] = new Thread(n1.sumar);
-                hilos[1].Start();
-
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (hilos[0].IsAlive)
-                hilos[0].Abort();
-            else
-            {
-                Numero n1 = new Numero(1);
-                n1.Num = int.Parse(txt1.Text);
-                n1.MuestraNumeroEvent += mostrar;
-
-
-                hilos[0] = new Thread(n1.sumar);
-                hilos[0].Start();
-
-            }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (hilos[2].IsAlive)
-                hilos[2].Abort();
-            else
-            {
-                Numero n1 = new Numero(3);
-                n1.Num = int.Parse(txt3.Text);
-                n1.MuestraNumeroEvent += mostrar;
-
-
-                hilos[2] = new Thread(n1.sumar);
-                hilos[2].Start();
-
-            }
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if (hilos[3].IsAlive)
-                hilos[3].Abort();
-            else
-            {
-                Numero n1 = new Numero(4);
-                n1.Num = int.Parse(txt4.Text);
-                n1.MuestraNumeroEvent += mostrar;
-
-
-                hilos[3] = new Thread(n1.sumar);
-                hilos[3].Start();
-
-            }
-        }
     }
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      foreach (Thread t in hilos)
+      {
+        if (t.IsAlive)
+          t.Abort();        
+
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      if (hilos[1].IsAlive)
+        hilos[1].Abort();
+      else
+      {
+        Numero n1 = new Numero(2);
+        n1.Num = int.Parse(txt2.Text);
+        n1.MuestraNumeroEvent += mostrar;
+
+
+        hilos[1] = new Thread(n1.sumar);
+        hilos[1].Start();
+
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (hilos[0].IsAlive)
+        hilos[0].Abort();
+      else
+      {
+        Numero n1 = new Numero(1);
+        n1.Num = int.Parse(txt1.Text);
+        n1.MuestraNumeroEvent += mostrar;
+
+
+        hilos[0] = new Thread(n1.sumar);
+        hilos[0].Start();
+
+      }
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      if (hilos[2].IsAlive)
+        hilos[2].Abort();
+      else
+      {
+        Numero n1 = new Numero(3);
+        n1.Num = int.Parse(txt3.Text);
+        n1.MuestraNumeroEvent += mostrar;
+
+
+        hilos[2] = new Thread(n1.sumar);
+        hilos[2].Start();
+
+      }
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      if (hilos[3].IsAlive)
+        hilos[3].Abort();
+      else
+      {
+        Numero n1 = new Numero(4);
+        n1.Num = int.Parse(txt4.Text);
+        n1.MuestraNumeroEvent += mostrar;
+
+
+        hilos[3] = new Thread(n1.sumar);
+        hilos[3].Start();
+
+      }
+    }
+  }
 }
